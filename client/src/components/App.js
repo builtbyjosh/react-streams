13d635c034020447041189dba7a9d0a3c6cad267
builtbyjosh <pc.refuge@gmail.com>
2021-06-17 22:24:53 -0500

    utilize Link over a tag

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const PageOne = () => {
-  return <div>PageOne</div>;
+  return (
+    <div>
+      Page One
+      <Link to="/pagetwo">Page Two</Link>
+    </div>);
 };
 const PageTwo = () => {
-  return <div>PageTwo</div>;
+  return (
+    <div>      
+      <Link to="/">Page One</Link>
+      Page Two
+    </div>);
 };
 
 const App = () => {
