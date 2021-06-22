902f73964613edfdc029c627603efcf7733cb5d4
builtbyjosh <pc.refuge@gmail.com>
2021-06-21 23:46:50 -0500

    initial OAuth2 setup

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "838870162700-phkkpfp9qbs45t4855ahgk35c39upfg9.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
