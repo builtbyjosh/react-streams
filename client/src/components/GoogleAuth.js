fcaca14a219e6233bf6eab42b4cad39ce01c1f68
builtbyjosh <pc.refuge@gmail.com>
2021-06-24 23:07:28 -0500

    wired up click event to work with sign in and out on OAuth2

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -18,21 +18,36 @@ class GoogleAuth extends React.Component {
         });
     });
   }
-// gapi.auth2.getAuthInstance().signOut()
 
   onAuthChange = () => {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
+
+  onSignIn = () => {
+    this.auth.signIn()
+  }
+
+  onSignOut = () => {
+    this.auth.signOut()
+  }
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        button.
+        <button className="ui red google button" onClick={this.onSignOut}>
+          <i className="google icon" />
+          Sign Out
+        </button>
       );
     } else {
-      return <div>I am not signed in</div>;
+      return (
+        <button className="ui red google button" onClick={this.onSignIn}>
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
 
