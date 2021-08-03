23c9210527bc0d9d90e2db45a6160f7332de907a
builtbyjosh <pc.refuge@gmail.com>
2021-08-02 21:36:07 -0500

    added lodash and connected streamReducer

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -5,23 +5,23 @@ import {
   EDIT_STREAM,
   DELETE_STREAM,
 } from "../actions/types";
+import _ from "lodash";
 
-
-const streamReducer =  (state = {}, action) => {
+const streamReducer = (state = {}, action) => {
   switch (action.type) {
-    case FETCH_STREAM:      
-      return { ...state, [action.payload.id]: action.payload}
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
     case FETCH_STREAMS:
-      return
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case CREATE_STREAM:
-      return {...state, [action.payload.id]: action.payload}
+      return { ...state, [action.payload.id]: action.payload };
     case EDIT_STREAM:
-      return {...state, [action.payload.id]: action.payload}
+      return { ...state, [action.payload.id]: action.payload };
     case DELETE_STREAM:
-      return
+      return _.omit(state, action.payload);
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default streamReducer
+export default streamReducer;
