a974683a10acef0e9ef519550479c2ea0b9aa071
builtbyjosh <pc.refuge@gmail.com>
2021-08-02 22:38:31 -0500

    created custom history, added to Router

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -26,6 +26,8 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const { userId } = getState().auth
   const response = await streams.post("/streams", {...formValues, userId});
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  
+  
 };
 
 export const fetchStreams = () => async (dispatch) => {
