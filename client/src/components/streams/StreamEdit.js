d4441fd5688f76c2bbf888518bfadfca977a571c
builtbyjosh <pc.refuge@gmail.com>
2021-08-03 22:16:28 -0500

    connect stream store to components

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React,{ Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+import { Field, reduxForm } from "redux-form";
 
-const StreamEdit = () => {
-  return (
-    <div>
-      Stream Edit
-    </div>
-  );
+class StreamEdit extends Component {
+  componentDidMount(){
+    this.props.fetchStream(this.props.match.params.id)
+  }
+
+  render() {
+    if (!this.props.stream){
+      return <div>Loading....</div>
+    }
+    return (
+      <div>Edit stream</div>
+    )
+  }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return {stream: state.streams[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, {fetchStream})(StreamEdit);
